334ea1b5603f7cb217509fc4f7cf135dd625b714
SteveTed0 <111440235+SteveTed0@users.noreply.github.com>
2023-09-10 15:58:34 +0900

    Update base.js

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
 @import "jquery-3.7.1.min.js";
-@import "jquery-migrate-1.4.1.min.js"
-@import "jquery-migrate-3.4.0.min.ks"
-@import "jquery-browser.js"
-@import "jquery.iframe-auto-height.plugin.1.9.5.js"
-@import "menubarimport.js"
+@import "jquery-migrate-1.4.1.min.js";
+@import "jquery-migrate-3.4.0.min.ks";
+@import "jquery-browser.js";
+@import "jquery.iframe-auto-height.plugin.1.9.5.js";
+@import "menubarimport.js";
